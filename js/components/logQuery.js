e0427d8c9eccf6d2245f1b641f311d6dc8d0e7b9
bandeanlorand <bandeanlori@gmail.com>
2025-07-01 18:40:22 +0300

    removed anon key

--- js/components/logQuery.js
+++ js/components/logQuery.js
@@ -1,8 +1,8 @@
 // logQuery.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://lktloyqtzbrkifexzqpo.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrdGxveXF0emJya2lmZXh6cXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNzcwNjksImV4cCI6MjA2Njk1MzA2OX0.vrkMq1i63KL6-HbGdqa-ZhtSjBv1cDfYZKbaLzVEgPs'; // anon key
+const SUPABASE_URL = 'https://lktloyqtzbrkifexzqpo.supabase.co';  // Supabase URL
+const SUPABASE_ANON_KEY = ''; // anon key
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
